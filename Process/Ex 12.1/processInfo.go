43f70898eb830c8e63a64c9f06d1f7a7dd31af24
DurzooBlint <marcin.karpik@gmail.com>
2020-05-17 15:17:12 -0700

    update

--- Process/Ex 12.1/processInfo.go
+++ Process/Ex 12.1/processInfo.go
@@ -8,6 +8,14 @@ import (
 	"syscall"
 )
 
+func fileStatFromInfoOs(fi os.FileInfo, flags *uint32, fileStat *FileStat) {
+	if statt, ok := fi.Sys().(*syscall.Stat_t); ok {
+		*flags |= ssh_FILEXFER_ATTR_UIDGID
+		fileStat.UID = statt.Uid
+		fileStat.GID = statt.Gid
+	}
+}
+
 func main() {
 
 	files, err := ioutil.ReadDir("/proc")
@@ -17,12 +25,12 @@ func main() {
 
 	for _, file := range files {
 		fmt.Println(file.Name())
-		fileNfo, err := os.Stat(file.Name())
+		/*fileNfo, err := os.Stat(file.IsDir())
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
-		fmt.Println(fileNfo.Sys().(*syscall.Stat_t).Uid)
+		fmt.Println(fileNfo.Sys().(*syscall.Stat_t).Uid)*/
 	}
 
 }
